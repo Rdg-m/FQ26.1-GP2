a3edfad265f94aa462dc4a21bdd2b6d5789c24f9
Rdg-m <182176501+Rdg-m@users.noreply.github.com>
2026-05-01 11:47:19 -0300

    b&h, MA, EMA

--- src/backtesting/modelos_pre_implementados.py
+++ src/backtesting/modelos_pre_implementados.py
@@ -1,7 +1,8 @@
 """
 esse arquivo será usado para guardar o código de modelos financeiros básicos para uso de teste das funções do projeto e benchmark
-ele será chamado por backtest_main
-reponderá a backtest_main tambem com sinais de compra e venda de ativos conforme documentado na documentação do backtest
+
+Vamos tratar os modelos aqui como modelos que seriam implementados para uso do código que estamos escrevendo, então faz sentido que eles chamem os métodos que desenvolveremos
+Ou eles sejam usados pelo backtest, depende da lógica de OOP.
 """
 
 from typing import Any
@@ -10,7 +11,11 @@ from typing import Any
 class estrat:
     def __init__(self, saldo) -> None:
         self.saldo = saldo
-        self. memoria = {}
+        self. memoria = None
+
+
+    def com(*a):
+        pass
 
 
 class buy_and_hold(estrat):
@@ -34,14 +39,48 @@ class buy_and_hold(estrat):
 
 class MA(estrat):
     ''' comprar e vender a depender da moving AVG de cada asset'''
-    def __init__(self, saldo) -> None:
+    def __init__(self, saldo, MA_compra, MA_venda) -> None:
         super().__init__(saldo)
+        self.politica = (MA_compra, MA_venda)
     
-    def com(*a):
+    def com(self, *a):
         self.add_mem(*a) #coloca preço na memória para calc MA
+        
+        if (p:= self.decisao()) is True:
+            # retornar buy orders
+            pass
+        elif p is False:
+            # retornar sell orders
+            pass
+        else:
+            #fazer nada
+            return None
+
+    def calc_MA(self):
+        #testar se já está calculado
         pass
 
+    def add_mem(self, *a):
+        #lembrar de reforçar invariantes
+        pass
+
+    def decisao(self):
+        '''retorna comprar ou vender ou esperar'''
+        MA = self.calc_MA()
+        compra, venda = self.politica
+
+        if MA>compra:
+            return True
+        
+        if MA<venda:
+            return False
+        
+        return None
 
 
-
-
+class EMA(MA):
+    '''mesma estrat de MA, mas com moving AVG exponencial'''
+    def calc_MA(self):
+        #calc moving AVG exp
+        pass
+         
