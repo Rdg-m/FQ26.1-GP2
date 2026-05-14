fe73402bfa0e7d4af23f57de14c5d9ea49b15e76
Pedro Tsuchie <pedro.tsuchie_usp@usp.br>
2026-05-14 08:10:26 -0300

    Modulo do backtesting feito. Algumas questoes a serem discutidos quanto a resposnsabilidade do modelo gerador de sinais(deve olhar a posicao atual? devemos informar isso ao modelo?

--- src/backtesting/backtesting_main.py
+++ src/backtesting/backtesting_main.py
@@ -286,3 +286,160 @@ def Atualizar_com_browniano(*a):
 
     return f(Step, *a)
 
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from src.dataprocessing.load import load_data
+from src.dataprocessing.clean import clean_data
+class BacktestEngine:
+    def __init__(self,data:pd.DataFrame,symbols:list,initial_capital:float=10000.0,commission:float=0.001):
+        self.data=data
+        self.symbols=symbols
+        self.commission=commission
+        self.slipagge=0.0
+
+        self.initial_capital=initial_capital
+        self.cash=initial_capital
+        self.portfolio_value=initial_capital
+        
+        self.open_positions={}
+        self.closed_positions=[] ##lista de dicionarios
+        self.daily_history=[]    ##saldo de cada dia para fazermos os graficos
+
+
+    def run(self,strategy_instance):
+        self.data=self.data.sort_index()
+        datas_unicas=self.data.index.unique()
+        for date in datas_unicas:
+            ohlcv=self.data.loc[self.data.index==date]
+            sinais=strategy_instance.generate_signals(ohlcv)
+            for sinal in sinais:
+                tipo=sinal["signal_type"]
+                simbolo=sinal["symbol"]
+                preco=sinal["price"]
+                qtd=sinal['quantity']
+                tempo=sinal["timestamp"]
+                stop_loss=sinal["stop_loss"]
+                take_profit=sinal["take_profit"]
+                reason=sinal.get("reason")
+                confidence=sinal.get("confidence")
+                indicators=sinal.get("indicators")
+                if tipo=="BUY":
+                    if self.cash>=preco*qtd:
+                        self._execute_buy(sinal,date)
+                elif tipo=="SELL":
+                    if simbolo in self.open_positions:
+                        self._execute_sell(sinal,date)
+            vendas_forcadas=[]
+            for simbolo,posicao in self.open_positions.items():
+                dados_acao=ohlcv[ohlcv["symbol"]==simbolo]
+                if dados_acao.empty:
+                    continue
+                minima=dados_acao["low"].iloc[0]
+                maxima=dados_acao["high"].iloc[0]
+                stop = posicao.get('stop_loss', 0.0)
+                alvo = posicao.get('take_profit', 0.0)
+                if stop>0 and minima<= stop:
+                    sinal_venda = {
+                        "symbol": simbolo,
+                        "price": stop, 
+                        "quantity": posicao['quantity'], 
+                        "reason": "Stop Loss Atingido"
+                    }
+                    vendas_forcadas.append(sinal_venda)
+                elif alvo > 0 and maxima >= alvo:
+                    sinal_venda = {
+                        "symbol": simbolo,
+                        "price": alvo,
+                        "quantity": posicao['quantity'], 
+                        "reason": "Take Profit Atingido"
+                    }
+                    vendas_forcadas.append(sinal_venda)
+            for sinal in vendas_forcadas:
+                self._execute_sell(sinal, date)
+            #analise do dia
+            valor_acoes = 0
+
+            for simbolo, posicao in self.open_positions.items():
+                dados_da_acao = ohlcv[ohlcv['symbol'] == simbolo]
+                
+                if not dados_da_acao.empty:
+                    preco_fechamento = dados_da_acao['close'].iloc[0]
+                    posicao['value'] = posicao['quantity'] * preco_fechamento
+                
+                valor_acoes += posicao['value']
+                
+            self.portfolio_value = self.cash + valor_acoes
+            
+            self.daily_history.append({
+                'date': date,
+                'cash': self.cash,
+                'portfolio_value': self.portfolio_value
+            })
+
+
+    def _execute_buy(self,sinal,date):
+        custo=sinal["price"]*sinal["quantity"]
+        custo=custo+custo*self.commission
+        self.cash=self.cash-custo
+        if sinal["symbol"] in self.open_positions:
+            qtd_antiga=self.open_positions[sinal["symbol"]]["quantity"]
+            preco_antigo=self.open_positions[sinal["symbol"]]["entry_price"]
+            qtd_nova=sinal["quantity"]
+            preco_novo=sinal["price"]
+            qtd_total = qtd_antiga + qtd_nova
+            preco_medio = ((qtd_antiga * preco_antigo) + (qtd_nova * preco_novo)) / qtd_total
+            self.open_positions[sinal["symbol"]]["quantity"] = qtd_total
+            self.open_positions[sinal["symbol"]]["entry_price"] = preco_medio
+            self.open_positions[sinal["symbol"]]["value"] = qtd_total * preco_medio
+            self.open_positions[sinal["symbol"]]["date"]=date
+            self.open_positions[sinal["symbol"]]["stop_loss"] = sinal.get("stop_loss", 0.0)
+            self.open_positions[sinal["symbol"]]["take_profit"] = sinal.get("take_profit", 0.0)
+        else:
+            self.open_positions[sinal["symbol"]] = {
+                'symbol': sinal["symbol"],
+                'quantity': sinal["quantity"],
+                'entry_price': sinal["price"],
+                'entry_date': date,
+                'stop_loss': sinal.get("stop_loss", 0.0),
+                'take_profit': sinal.get("take_profit", 0.0),
+                'entry_reason': sinal.get("reason", "N/A"),
+                'value': sinal["price"] * sinal["quantity"]
+            }
+
+
+    def _execute_sell(self, sinal, date):
+        simbolo = sinal["symbol"]
+        preco_venda = sinal["price"]
+        
+        posicao = self.open_positions[simbolo]
+        qtd_venda = min(sinal["quantity"], posicao["quantity"])
+        preco_entrada = posicao["entry_price"]
+        data_entrada = posicao["entry_date"]
+        
+        valor_bruto_venda = preco_venda * qtd_venda
+        custo_comissao = valor_bruto_venda * self.commission
+        valor_liquido_venda = valor_bruto_venda - custo_comissao
+        
+        self.cash = self.cash + valor_liquido_venda
+
+        custo_da_venda = qtd_venda * preco_entrada 
+        lucro_liquido = valor_liquido_venda - custo_da_venda
+        
+        trade_fechado = {
+            'symbol': simbolo,
+            'entry_date': data_entrada,
+            'entry_price': preco_entrada,
+            'exit_date': date,
+            'exit_price': preco_venda,
+            'quantity': qtd_venda,
+            'net_profit': lucro_liquido,
+            'exit_reason': sinal.get("reason", "Sinal da Estratégia")
+        }
+        self.closed_positions.append(trade_fechado)
+        
+        posicao["quantity"] = posicao["quantity"] - qtd_venda
+        posicao["value"] = posicao["quantity"] * preco_entrada
+        if posicao["quantity"] <= 0:
+            del self.open_positions[simbolo]
+
