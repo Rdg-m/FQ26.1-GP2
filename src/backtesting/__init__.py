dd8305689ee5e0acc985bbcc2271d3ea2ba2c6f4
Rdg-m <182176501+Rdg-m@users.noreply.github.com>
2026-06-02 20:49:56 -0300

    confundi os nomes dos arquivos (roll-back do ultimo commit)

--- src/backtesting/__init__.py
+++ src/backtesting/__init__.py
@@ -1,4 +1,33 @@
 """Backtesting package."""
 from .backtesting_main import BacktestEngine
+from .estrategy import (
+    adx,
+    sma,
+    ema,
+    macd,
+    rsi,
+    stochastic,
+    roc,
+    momentum,
+    bollinger_bands,
+    atr,
+    obv,
+    volume_roc,
+    mfi,
+)
 
-__all__ = ["BacktestEngine"]
+__all__ = [
+    "adx",
+    "sma",
+    "ema",
+    "macd",
+    "rsi",
+    "stochastic",
+    "roc",
+    "momentum",
+    "bollinger_bands",
+    "atr",
+    "obv",
+    "volume_roc",
+    "mfi",
+    "BacktestEngine"]
